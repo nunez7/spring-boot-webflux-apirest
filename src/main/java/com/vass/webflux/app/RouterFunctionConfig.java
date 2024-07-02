cd85a1c2a8e11fefe9624b0ccf94f43aa38f3f19
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-07-01 17:48:18 -0700

    API Rest usando Functional Endpoints

--- src/main/java/com/vass/webflux/app/RouterFunctionConfig.java
+++ src/main/java/com/vass/webflux/app/RouterFunctionConfig.java
@@ -13,10 +13,16 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 
 @Configuration
 public class RouterFunctionConfig {
-		
-	@Bean
-	public RouterFunction<ServerResponse> router(ProductoHandler handler){
-		return route(GET("/api/v2/productos"), request -> handler.listar(request));
+
+    @Bean
+    RouterFunction<ServerResponse> router(ProductoHandler handler){
+		return route(GET("/api/v2/productos"), handler::listar)
+				.andRoute(GET("/api/v2/productos/{id}"), handler::ver)
+				.andRoute(POST("/api/v2/productos"), handler::crear)
+				.andRoute(PUT("/api/v2/productos/{id}"), handler::editar)
+				.andRoute(DELETE("/api/v2/productos/{id}"), handler::eliminar)
+				.andRoute(POST("/api/v2/productos/upload/{id}"), handler::upload)
+				.andRoute(POST("/api/v2/productos/crear"), handler::crearConFoto);
 	}
 
 }
