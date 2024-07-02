a1bf06c604b3e8536511540edd7173ed261838ab
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-07-01 21:52:11 -0700

    Test endpoint listar

--- src/test/java/com/vass/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/vass/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,30 @@
 package com.vass.webflux.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.vass.webflux.app.models.documents.Producto;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
+	
+	@Autowired
+	private WebTestClient client;
 
 	@Test
 	void contextLoads() {
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBodyList(Producto.class)
+		.hasSize(10);
 	}
 
 }
